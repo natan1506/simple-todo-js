832d75dcb57304b6ceb60ea4073119c6fcdfcc45
Natan Souza <42328139+natan1506@users.noreply.github.com>
2022-03-23 12:10:34 -0300

    complete

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 (function(){
-  const todos = localStorage.getItem('todos@local');
+  const todos = JSON.parse(localStorage.getItem('todos@local'));
 
-  console.log(!!todos)
-
-  
+  if(todos){
+    generateList(todos)
+  }  
 })()
 
 function insertTodo() {
   const input = document.getElementById('todo')
   console.log(input.value)
+
+  const todos = localStorage.getItem('todos@local') ? JSON.parse(localStorage.getItem('todos@local')) : [];
+  
+  todos.push(input.value)
+  localStorage.setItem('todos@local', JSON.stringify(todos));
+  generateList(todos);
   input.value = '';
   
+}
 
+function generateList(todos){
+  const divListTodo = document.getElementById('listTodo');
+  divListTodo.innerHTML = ""
+
+  for(i = 0; i < todos.length; i++){
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <span>${todos[i]}</span>
+      <button onclick="removeTodo(${i})">X</button>
+    `
+    divListTodo.appendChild(li)
+  }
+}
+
+function removeTodo(index){
+  const todos = JSON.parse(localStorage.getItem('todos@local'));
+  todos.splice(index, 1);
+
+  localStorage.setItem('todos@local', JSON.stringify(todos));
+  generateList(todos);
 }
